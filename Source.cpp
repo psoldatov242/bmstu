0ce72a74b6476ace03dd3d0a505c374225b44e6c
psoldatov242 <psoldatov242@gmail.com>
2025-09-06 11:28:21 +0300

    first commit

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/*int main()
+{
+    float a;
+    std::cout << "Enter today's temperature:";
+    std::cin >> a;
+    if (a < 0.0)
+        std::cout << "We recommend you to wear winter clothes";
+    if ((0.0 <= a) and (a < 11.0))
+        std::cout << "We recommend you to wear warm clothes";
+    if ((11.0 <= a) and (a < 20.0))
+        std::cout << "We recommend you light clothing";
+    if (a > 20.0)
+        std::cout << "We recommend you to wear summer clothes";
+    return 0;
+}
+*/
+
+/*int main()
+{
+    int a, b;
+    b = 0;
+    std::cout << "Type amount bus stops ";
+    std::cin >> a;
+    for (int i = 0; i < a; i++)
+        b += 5;
+    std::cout << b;
+}
+*/
